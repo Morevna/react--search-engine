aa640b6ba6ff5e40fb45f76c0adb9758dd000a38
Breslavets Maria <alwayslearn@mail.ru>
2026-06-08 22:48:44 +0300

    feat: add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev
 export default defineConfig({
   plugins: [react()],
+  base: '/react--search-engine/',
   test: {
     globals: true,
     environment: 'jsdom',
