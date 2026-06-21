c21a5a2baea3232571b5550a860c20f6f20a9bfd
Breslavets Maria <alwayslearn@mail.ru>
2026-06-21 19:06:08 +0300

    feat: Next.js installation and setup

---
+++ nextjs-ssr/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>
+        React Search Engine migrated to Next.js
+      </h1>
+    </main>
+  );
+}
---
+++ nextjs-ssr/src/components/PokemonCard.tsx
@@ -0,0 +1,50 @@
+import type { Pokemon } from '../App';
+import { usePokemonStore } from '../store/usePokemonStore';
+
+interface Props {
+  pokemon: Pokemon;
+  onClick?: () => void;
+}
+
+const PokemonCard = ({ pokemon, onClick }: Props) => {
+  const { selected, toggle } = usePokemonStore();
+  const isChecked = selected.some((p) => p.name === pokemon.name);
+
+  return (
+    <div
+      onClick={onClick}
+      style={{
+        border: '1px solid #ddd',
+        borderRadius: '10px',
+        padding: '15px',
+        textAlign: 'center',
+        background: '#fafafa',
+        cursor: 'pointer',
+        position: 'relative',
+      }}
+    >
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={(e) => {
+          e.stopPropagation();
+          toggle(pokemon);
+        }}
+        onClick={(e) => e.stopPropagation()}
+        style={{
+          position: 'absolute',
+          top: '10px',
+          left: '10px',
+          zIndex: 100,
+          cursor: 'pointer',
+        }}
+      />
+
+      <img src={pokemon.image} alt={pokemon.name} width="120" />
+      <h3>{pokemon.name}</h3>
+      <p>{pokemon.description}</p>
+    </div>
+  );
+};
+
+export default PokemonCard;
