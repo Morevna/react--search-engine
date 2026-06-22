45c89ae5464ba6b6428c42d8ff88bd1db1cb6174
Breslavets Maria <alwayslearn@mail.ru>
2026-06-22 23:41:39 +0300

    fix: fix deploy

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin(
-  './src/i18n/request.ts' // Путь к твоему файлу request.ts
+  './src/i18n/request.ts'
 );
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Твои другие настройки (если есть), например для картинок:
+  // 
   images: {
     unoptimized: true,
   },
